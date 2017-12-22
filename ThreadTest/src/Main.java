cc240550a188b74d7546fe54e202eaec9e29b3c1
Andrey Mostovykh <andrey.mostovykh@telenordigital.com>
2017-12-22 15:59:47 +0100

    Move threads list obtaining to the proper place in the shutdown hook. Use .equals for strings comparison

--- ThreadTest/src/Main.java
+++ ThreadTest/src/Main.java
@@ -6,27 +6,26 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //List<TestThread> threadList = new ArrayList<>();
 
         for (int i = 0; i < 4; i++) {
             TestThread t = new TestThread();
-            //threadList.add(t);
             t.start();
         }
 
         Runtime.getRuntime().addShutdownHook(new Thread() {
 
-            Set<Thread> threadSet = Thread.getAllStackTraces().keySet();
-
             public void run() {
+
+                Set<Thread> threadSet = Thread.getAllStackTraces().keySet();
+
                 for (Thread t : threadSet) {
-                    if (t.getClass().getName() == "TestThread") {
+                    if (t.getClass().getName().equals("TestThread")) {
                         t.interrupt();
                     }
                 }
 
                 for (Thread t : threadSet) {
-                    if (t.getClass().getName() == "TestThread") {
+                    if (t.getClass().getName().equals("TestThread")) {
                         try {
                             t.join();
                         } catch (InterruptedException e) {
